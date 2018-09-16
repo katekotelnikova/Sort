5acdd7f266486ca2eae8cc7f82d8c4f4bb46e05d
Катрин <www.maleckiy33@gmail.com>
2018-09-16 16:59:22 +0300

    Реализовано считывание данных из исходного файла, а также изменена реализация подсчёта повторений

--- src/ru/kev/sort/Main.java
+++ src/ru/kev/sort/Main.java
@@ -1,6 +1,13 @@
 package ru.kev.sort;
 
-import java.util.HashMap;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+
 
 /**
  * Класс для сортировки массива по количеству повторений числа в массиве
@@ -10,20 +17,62 @@ import java.util.HashMap;
 public class Main {
 
     public static void main(String[] args) {
-        int mas[] = {10, 12, 12, 2, 3, 3};
 
-        HashMap<Integer, Integer> uniqueValues = new HashMap<>();
+        ArrayList<Integer> numbers = new ArrayList<>();
 
-        for (int i = 0; i < mas.length; i++) {
-            int count = 0;
-            for (int j = 0; j < mas.length; j++) {
-                if (mas[i] == mas[j]) {
-                    count++;
-                }
+        String s;
+        try (BufferedReader reader = new BufferedReader(new FileReader("input.txt"))) {
+            s = reader.readLine();
+            for (String num : s.split(" ")) {
+                numbers.add(Integer.parseInt(num));
             }
-            uniqueValues.put(mas[i], count);
+
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        System.out.println(uniqueValues);
+
+
+        Map<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
+
+        for (Integer key : numbers) {
+            if (hashMap.keySet().contains(key)) {
+                hashMap.put(key, hashMap.get(key) + 1);
+            } else {
+                hashMap.put(key, 1);
+            }
+        }
+        System.out.println(hashMap);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Set<Map.Entry<Integer, Integer>> set = hashMap.entrySet();
+//
+//
+//        int sortMas[] = new int[mas.length];
+//            //for (int j = 0; j < sortMas.length; j++) {
+//        for (Map.Entry<Integer, Integer> val : set) {
+//                for (int i = 0; i < val.getValue(); i++) {
+//                    //sortMas[j] = val.getKey();
+//                    System.out.print(val.getKey() + " ");
+//
+//                }
+//            }
+
     }
 }
 
