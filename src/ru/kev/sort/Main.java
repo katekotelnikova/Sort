f3f0fe0740d379e97fc34bd5f5580acbc3810a2e
Катрин <www.maleckiy33@gmail.com>
2018-09-22 10:16:45 +0300

    реализована сортировка

--- src/ru/kev/sort/Main.java
+++ src/ru/kev/sort/Main.java
@@ -1,31 +1,67 @@
 package ru.kev.sort;
 
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
-
 
 /**
  * Класс для сортировки массива по количеству повторений числа в массиве
  *
  * @author Kotelnikova E. group 15it18
  */
+
 public class Main {
 
     public static void main(String[] args) {
 
-        ArrayList<Integer> numbers = input();
+        ArrayList<Integer> numbers = readFile();
 
         Map<Integer, Integer> hashMap = freq(numbers);
+
+        System.out.println("Сортируем числа...");
+        SortedSet<Map.Entry<Integer, Integer>> set = sortedNumbers(hashMap);
+
+        writeInFile(set);
+    }
+
+    /**
+     * Метод для записи отсортированных чисел в файл.
+     * @param set множество чисел
+     */
+    private static void writeInFile(SortedSet<Map.Entry<Integer, Integer>> set) {
+        try (FileWriter writer = new FileWriter("output.txt")) {
+            for (Map.Entry<Integer, Integer> val : set) {
+                for (int i = 0; i < val.getValue(); i++) {
+                    writer.write(val.getKey() + " ");
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
     /**
+     * Метод возвращает набор чисел, отсортированных по количеству повторений
+     *
+     * @param hashMap набор данных в виде: число-количество повторений
+     * @return набор чисел
+     */
+    private static SortedSet<Map.Entry<Integer, Integer>> sortedNumbers(Map<Integer, Integer> hashMap) {
+        SortedSet<Map.Entry<Integer, Integer>> set = new TreeSet<>(Map.Entry.<Integer, Integer>comparingByValue().reversed());
+        set.addAll(hashMap.entrySet());
+
+        System.out.println(set);
+        return set;
+    }
+
+
+    /**
      * Метод подсчитывает количество повторений числа в списке и
      * сохраняет в hashMap в виде: ключ(число) - значение(кол-во повторений)
+     *
      * @param numbers список чисел
      * @return hashMap
      */
@@ -44,9 +80,10 @@ public class Main {
 
     /**
      * Метод для считывания данных из файла в Array List.
+     *
      * @return Array List, содержащий числа из файла
      */
-    private static ArrayList<Integer> input() {
+    private static ArrayList<Integer> readFile() {
         ArrayList<Integer> numbers = new ArrayList<>();
         String s;
         try (BufferedReader reader = new BufferedReader(new FileReader("input.txt"))) {
@@ -60,6 +97,7 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
         return numbers;
     }
 }
